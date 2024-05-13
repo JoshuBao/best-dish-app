9aa45404fe40f8afc11357e7900c6894731cc908
JoshuBao <Joshuaicheng@gmail.com>
2024-05-13 01:30:18 -0700

    Initial Commit

---
+++ best-dish-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  best-dish-app
+//
+//  Created by Joshua Cheng on 5/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ best-dish-app/best_dish_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  best_dish_appApp.swift
+//  best-dish-app
+//
+//  Created by Joshua Cheng on 5/13/24.
+//
+
+import SwiftUI
+
+@main
+struct best_dish_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
