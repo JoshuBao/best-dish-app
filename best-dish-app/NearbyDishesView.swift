439a40b16837daa8fd2c912c4e5bf21be00e3c12
JoshuBao <Joshuaicheng@gmail.com>
2024-05-14 16:48:28 -0700

    View images

--- best-dish-app/NearbyDishesView.swift
+++ best-dish-app/NearbyDishesView.swift
@@ -14,25 +14,54 @@ struct NearbyDishesView: View {
     
     var body: some View {
         NavigationView {
-            List(viewModel.labeledImages) { labeledImage in
-                HStack {
-                    if let uiImage = UIImage(data: labeledImage.imageData) {
-                        Image(uiImage: uiImage)
-                            .resizable()
-                            .aspectRatio(contentMode: .fill)
-                            .frame(width: 60, height: 60)
-                            .clipped()
-                    }
-                    
-                    VStack(alignment: .leading) {
-                        Text(labeledImage.foodDish)
-                        Text(labeledImage.location)
-                            .font(.caption)
-                            .foregroundColor(.gray)
+            List(viewModel.labeledImages, id: \.id) { labeledImage in  // Ensure LabeledImage conforms to Identifiable
+                NavigationLink(destination: DishDetailView(labeledImage: labeledImage)) {
+                    HStack {
+                        if let uiImage = UIImage(data: labeledImage.imageData) {
+                            Image(uiImage: uiImage)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: 60, height: 60)
+                                .clipped()
+                        }
+                        
+                        VStack(alignment: .leading) {
+                            Text(labeledImage.foodDish)
+                            Text(labeledImage.location)
+                                .font(.caption)
+                                .foregroundColor(.gray)
+                        }
                     }
                 }
             }
             .navigationBarTitle("Best Dishes Near You")
         }
+    }
+}
+
+
+
+struct DishDetailView: View {
+    var labeledImage: LabeledImage
+    
+    var body: some View {
+        ScrollView {
+            VStack {
+                if let uiImage = UIImage(data: labeledImage.imageData) {
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .padding()
+                }
+                Text("Dish: \(labeledImage.foodDish)")
+                    .font(.title)
+                    .padding()
+                Text("Location: \(labeledImage.location)")
+                    .font(.subheadline)
+                    .foregroundColor(.gray)
+                    .padding()
+            }
+        }
+        .navigationBarTitle(Text(labeledImage.foodDish), displayMode: .inline)
     }
 }
